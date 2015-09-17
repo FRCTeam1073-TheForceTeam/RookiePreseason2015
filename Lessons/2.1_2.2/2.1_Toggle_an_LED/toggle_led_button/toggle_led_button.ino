e4de433393c0f4a2759978f9f80e9eff00b2fdc4
Errica Cheng <errica.cheng@gmail.com>
2015-09-16 20:25:31 -0400

    started working on OO for toggle_led_button

--- Lessons/2.1_2.2/2.1_Toggle_an_LED/toggle_led_button/toggle_led_button.ino
+++ Lessons/2.1_2.2/2.1_Toggle_an_LED/toggle_led_button/toggle_led_button.ino
@@ -5,8 +5,9 @@ last updated: 9/9/14
 Program: Toggles an LED using the breadboard button
 */
 
-int red = 5;
+/*int red = 5;
 int button = 7;
+
 
 void setup(){ //setup runs just once
   pinMode(red, OUTPUT); //establishes red as an output
@@ -19,4 +20,37 @@ int buttonVal = digitalRead(button);
   else{
     digitalWrite(red, LOW);
   }
+}*/
+
+class ButtonToggle {
+  public:
+   
+    ButtonToggle(int buttonVal);
+    void begin();
+    void toggleOn();
+    void toggleOff();
+    bool status();
+  private:
+    int _pin;
+    bool _status;
+    int red = 5;
+    int button = 7;
 }
+ButtonToggle::ButtonToggle(int buttonVal)
+void ButtonToggle::begin();
+//Put starting functions here (i.e. set pin to output/input)
+
+void ButtonToggle::toggleOn();
+//Method for toggling on
+
+void ButtonToggle::toggleOff();
+//Method for toggling off
+
+void setup(){
+  
+}
+
+void loop(){
+  
+}
+
