aafc0578ac797eb5ad8885ed10fc18b0004f6d35
Kyle Pittman <kyle.pittman.first@gmail.com>
2015-10-21 20:36:50 -0400

    2.1 button LED
    Alex Dogerty and Jackson Claire and Pittman the Goalie

--- Group D/Lessons/2.1_2.2/2.1_Toggle_an_LED/User_Program2_1/User_Program2_1.ino
+++ Group D/Lessons/2.1_2.2/2.1_Toggle_an_LED/User_Program2_1/User_Program2_1.ino
@@ -5,11 +5,29 @@ Method stubs are pre-made. add your own code to complete the challenge!
 Program Task: Have the LED on pin 5 turn on when the button is pressed
 Feel free to view the example program within the same folder if you need help!
 */
+#include "Led.h";
+#include "Button.h";
+
+ Led* redLed = new Led(5);
+ Led* greenLed = new Led(6);
+ Button* redButton = new Button(7);
 
 void setup(){
   //your code goes here
+  pinMode(5, OUTPUT);
+  pinMode(6, OUTPUT);
+  pinMode(7, INPUT);
+  redLed->turnOff();
+  greenLed->turnOff();
 }
 
 void loop(){
-  //also here
+  //if button is pressed turn on light
+  if( redButton->isPressed()){
+    redLed->turnOn(); 
+    greenLed->turnOff();
+  } else{
+    redLed->turnOff();
+    greenLed->turnOn();
+  }
 }
