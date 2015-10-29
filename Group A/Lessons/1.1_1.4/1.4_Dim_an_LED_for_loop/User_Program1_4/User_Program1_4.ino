773ed90489ff0dbdb64d2b95a04cc2f99de9f275
nnapior <nnapior@gmail.com>
2015-10-28 20:26:22 -0400

    blah

--- Group A/Lessons/1.1_1.4/1.4_Dim_an_LED_for_loop/User_Program1_4/User_Program1_4.ino
+++ Group A/Lessons/1.1_1.4/1.4_Dim_an_LED_for_loop/User_Program1_4/User_Program1_4.ino
@@ -5,11 +5,21 @@ Method stubs are pre-made. add your own code to complete the challenge!
 Program Task: Fade in and out the led on pin 5 using 2 for loops
 Feel free to view the example program within the same folder if you need help!
 */
+int red = 5;
+int del = 20;
 
 void setup(){
-  //your code goes here
+  pinMode(red, OUTPUT);
+  
 }
 
 void loop(){
-  //also here
+  for (int i = 0; i < 255; i += 5){
+    analogWrite(red, i);
+    delay(del);
+  }
+  for (int i = 255; i > 0; i -= 5){
+    analogWrite(red, i);
+    delay(del);
+  }
 }
--- Group A/Lessons/1.1_1.4/1.5_LED_Challenge/LED_challenge/LED_challenge.ino
+++ Group A/Lessons/1.1_1.4/1.5_LED_Challenge/LED_challenge/LED_challenge.ino
@@ -1,88 +1,64 @@
 // LED Challenge
-
+/* Cycle all of the leds to cycle and fade them instead 
+ *  of on and off... Use ''For'' loops!
+ */
 int red = 5;
-int green = 6;
 int yellow = 3;
+int green = 6;
+int del = 50;
 
-int blinkDelay = 15;
-int greenDelay = 10;
-int yellowDelay = 20;
+void cycle(int led){
+ for (int i = 0; i < 255; i += 5){
+  analogWrite(led, i);
+  delay(del);  
+ }
+ for (int i = 255; i > 0; i -= 5){
+  analogWrite(led, i);
+  delay(del);
+ }
+}
 
-// put your setup code here, to run once:
 void setup() {
-  
   pinMode(red, OUTPUT);
-  pinMode(green, OUTPUT);
   pinMode(yellow, OUTPUT);
-  
+  pinMode(green, OUTPUT);
 }
 
-// put your main code here, to run repeatedly:
+
 void loop() {
+  cycle(red);
+  cycle(yellow);
+  cycle(green
   
-  // start red LED
-  for(int i = 0; i < 255; i += 2) {
-    analogWrite(red, i);
-    delay(5);
-  }
-  // start green LED
-  for(int j = 0; j < 255; j += 2) {
-    //delay(greenDelay);
-    analogWrite(green, j);
-    delay(5);
-  }
   
-  for(int i = 2; i < 255; i += 4) {
-    analogWrite(red, i);
-    delay(5);
-  }
   
-  for(int i = 255; i >= 0; i -= 6) {
-    analogWrite(red, i);
-    delay(blinkDelay);
-  }
-  // green LED
-
-  for(int j = 255; j >= 0; j -= 6) {
-    analogWrite(green, j);
-    delay(blinkDelay);
-  }
-  // yellow LED
   
-  for(int k = 255; k >= 0; k -= 6) {
-    analogWrite(yellow, k);
-    delay(blinkDelay);
-  }
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  
+  ); 
+  
 }
-
-/*
-// red LED
-  for(int i = 0; i < 255; i += 5) {
-    analogWrite(red, i);
-    delay(blinkDelay);
-  }
-  for(int i = 255; i >= 0; i -= 5) {
-    analogWrite(red, i);
-    delay(blinkDelay);
-  }
-  // green LED
-  for(int j = 0; j < 255; j += 5) {
-    //delay(greenDelay);
-    analogWrite(green, j);
-    delay(blinkDelay);
-  }
-  for(int j = 255; j >= 0; j -= 5) {
-    analogWrite(green, j);
-    delay(blinkDelay);
-  }
-  // yellow LED
-  for(int k = 0; k < 255; k += 5) {
-    //delay(yellowDelay);
-    analogWrite(yellow, k);
-    delay(blinkDelay);
-  }
-  for(int k = 255; k >= 0; k -= 5) {
-    analogWrite(yellow, k);
-    delay(blinkDelay);
-  }
-*/
+  
+  
+  
