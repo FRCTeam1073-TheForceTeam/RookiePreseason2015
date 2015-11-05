313f0931f7836ce2638ef61d8a3630883a6077ff
jackSinclairSoftware <jack@sinclairfamily.net>
2015-11-04 20:26:20 -0500

    3_1

--- Group D/Lessons/3.1_3.4/3.1_Drive_Robot_Forward/User_Program3_1/User_Program3_1.ino
+++ Group D/Lessons/3.1_3.4/3.1_Drive_Robot_Forward/User_Program3_1/User_Program3_1.ino
@@ -1,3 +1,6 @@
+#include <Servo.h>
+
+#include <Drive.h>
 /*Software 1073 Arduino BoeBots
 User Program 
 Method stubs are pre-made. add your own code to complete the challenge!
@@ -5,11 +8,12 @@ Method stubs are pre-made. add your own code to complete the challenge!
 Program Task: drive the robot forward for a few seconds
 Feel free to view the example program within the same folder if you need help!
 */
-
+Drive* drive = new Drive(9, 10);
 void setup(){
-  //your code goes here
+  
 }
 
 void loop(){
-  //also here
+  drive->fwd(50);
+  
 }
