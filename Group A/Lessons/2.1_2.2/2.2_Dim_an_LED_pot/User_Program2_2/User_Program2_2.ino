ed07a37f3f1663f1c256e46c929e812edffcd451
Noblesix84 <hunterstickney@gmail.com>
2015-11-04 20:29:11 -0500

    Completed lesson 2

--- Group A/Lessons/2.1_2.2/2.2_Dim_an_LED_pot/User_Program2_2/User_Program2_2.ino
+++ Group A/Lessons/2.1_2.2/2.2_Dim_an_LED_pot/User_Program2_2/User_Program2_2.ino
@@ -1,3 +1,7 @@
+#include <Potentiometer.h>
+
+#include <Led.h>
+
 /*Software 1073 Arduino BoeBots
 User Program 
 Method stubs are pre-made. add your own code to complete the challenge!
@@ -5,11 +9,18 @@ Method stubs are pre-made. add your own code to complete the challenge!
 Program Task: Dim the led on pin 5 using the potentiometer, so that as the potentiometer is turned, the brightness is adjusted
 Feel free to view the example program within the same folder if you need help!
 */
+Led *myLed;
+Potentiometer *myPot;
 
 void setup(){
-  //your code goes here
+  myLed=new Led(5);
+  myPot=new Potentiometer(5);
+  
 }
 
 void loop(){
-  //also here
+  float value = myPot->getValue() / 1023.0;
+  int outValue = (int) (value * 255.0);
+  myLed->turnOnDim(outValue);
+  
 }
