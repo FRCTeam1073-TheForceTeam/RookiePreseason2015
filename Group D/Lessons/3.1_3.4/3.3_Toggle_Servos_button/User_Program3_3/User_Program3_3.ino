9b0abbacd9130dc63c372396d23ee9a30d11ebc9
Kyle Pittman <kyle.pittman.first@gmail.com>
2015-12-02 20:22:54 -0500

    Button Drive
    That

--- Group D/Lessons/3.1_3.4/3.3_Toggle_Servos_button/User_Program3_3/User_Program3_3.ino
+++ Group D/Lessons/3.1_3.4/3.3_Toggle_Servos_button/User_Program3_3/User_Program3_3.ino
@@ -1,3 +1,8 @@
+#include <Servo.h>
+#include <Drive.h>
+#include <Button.h>
+
+
 /*Software 1073 Arduino BoeBots
 User Program 
 Method stubs are pre-made. add your own code to complete the challenge!
@@ -6,10 +11,21 @@ Program Task: have the robot drive when the button is pressed
 Feel free to view the example program within the same folder if you need help!
 */
 
+#define BUTTON1 7
+ 
+Button* butt1;
+Drive* drive;
 void setup(){
   //your code goes here
-}
+  butt1 = new Button(BUTTON1);
 
+  drive = new Drive(10, 9);
+}
 void loop(){
   //also here
+  if(butt1->isPressed()) {
+    drive->fwd(50);
+  }else {
+    drive->stop();
+  }
 }
